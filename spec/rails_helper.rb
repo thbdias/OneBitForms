8644bd11542d4b346e68bb72a3ac88331ab749cb
thbdias <thiagobalbino91@gmail.com>
2019-03-27 12:48:54 -0300

    instalacao do devise token auth + ambiente de testes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 	
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
+config.include Devise::Test::ControllerHelpers, type: :controller
+
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
@@ -60,5 +62,5 @@ RSpec.configure do |config|
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
-  # config.filter_gems_from_backtrace("gem name")
+  # config.filter_gems_from_backtrace("gem name")  
 end
