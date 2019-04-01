f53c94715057b3aa551f679764f943e757ef4c49
thbdias <thiagobalbino91@gmail.com>
2019-04-01 17:39:39 -0300

    ajustando nossas factories

--- spec/factories/forms.rb
+++ spec/factories/forms.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :form do
-    title { "MyString" }
-    description { "MyText" }
-    user { nil }
-    primary_color { "MyString" }
-    enable { false }
+    #       module::classe.metodo
+    title { FFaker::Lorem.word }
+    description { FFaker::Lorem.word }
+    user
+    primary_color { "##{FFaker::Color.hex_code}" }
+    enable { FFaker::Boolean.maybe }
   end
 end
