ffd38a171c790583e75c75ff1bb684556eb59a8d
thbdias <thiagobalbino91@gmail.com>
2019-04-09 12:20:29 -0300

    ajuste nos testes do form

--- spec/requests/api/v1/forms_spec.rb
+++ spec/requests/api/v1/forms_spec.rb
@@ -47,16 +47,24 @@ RSpec.describe "Api::V1::Forms", type: :request do
       context "And is enable" do
         before do
           @form = create(:form, user: @user, enable: true)
+          @question1 = create(:question, form: @form)
+          @question2 = create(:question, form: @form)          
+          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)        
         end
  
         it "returns 200" do
-          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          # get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           expect_status(200)
         end
  
         it "returned Form with right datas" do
-          get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
+          # get "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           expect(json).to eql(JSON.parse(@form.to_json))
+        end
+
+        it "returned associated questions" do
+          expect(json['questions'].first).to eql(JSON.parse(@question1.to_json))
+          expect(json['questions'].last).to  eql(JSON.parse(@question2.to_json))
         end
       end
  
@@ -209,15 +217,16 @@ RSpec.describe "Api::V1::Forms", type: :request do
     end
 
     context "With valid authentication headers" do        
-      context "When form exists" do 
-        before do
-          @user = create(:user)
-        end
+      before do
+        @user = create(:user)
+      end
 
+      context "When form exists" do         
         # testando autorização
         context "And user is the owner" do
           before do
             @form = create(:form, user: @user)
+            @question = create(:question, form: @form)
             delete "/api/v1/forms/#{@form.friendly_id}", params: {}, headers: header_with_authentication(@user)
           end
   
@@ -227,6 +236,10 @@ RSpec.describe "Api::V1::Forms", type: :request do
   
           it "form are deleted" do
             expect(Form.all.count).to eql(0)
+          end
+
+          it "associated question are deleted" do
+            expect(Question.all.count).to eql(0)
           end
         end
   
@@ -243,11 +256,7 @@ RSpec.describe "Api::V1::Forms", type: :request do
         end
       end
   
-      context "When form dont exists" do
-        before do
-          @user = create(:user)
-        end
-
+      context "When form dont exists" do        
         it "returns 404" do
           delete "/api/v1/forms/#{FFaker::Lorem.word}", params: {}, headers: header_with_authentication(@user)
           expect_status(404)
